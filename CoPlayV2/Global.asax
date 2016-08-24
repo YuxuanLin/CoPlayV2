2e0b12cccdb72cbaf2f69e1c09125ca8facf05c3
linyuxuan <linyuxuan@LINYUXUANE6C0>
2016-08-25 00:52:14 +1000

    Add project files.

---
+++ CoPlayV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoPlayV2.MvcApplication" Language="C#" %>
---
+++ CoPlayV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoPlayV2.Startup))]
+namespace CoPlayV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
