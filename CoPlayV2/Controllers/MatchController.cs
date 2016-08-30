10653101aa9d89fee160eb0c44a6ab86ac939a51
linyuxuan <linyuxuan@WIN10@HOME>
2016-08-30 20:32:57 +1000

    Step2 function done

--- CoPlayV2/Controllers/MatchController.cs
+++ CoPlayV2/Controllers/MatchController.cs
@@ -1,10 +1,12 @@
 ﻿using CoPlayV2.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,22 +16,29 @@ namespace CoPlayV2.Controllers
     {
         private CoPlayDBModel db = new CoPlayDBModel();
 
-        private UserManager<ApplicationUser> userManager;
+        private ApplicationUserManager _userManager;
 
-        public MatchController()
+        public ApplicationUserManager UserManager
         {
-            userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            get
+            {
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
         }
 
         // GET: Match
         [Authorize]
         public ActionResult Index()
         {
-            return View("Step1");
+            return View("Index");
         }
 
         [Authorize]
-        public ActionResult Step1(MatchIndexViewModels model)
+        public async Task<ActionResult> Step2(MatchIndexViewModels model)
         {
 
             //Check model null
@@ -40,71 +49,23 @@ namespace CoPlayV2.Controllers
 
             //Search ids from usersportperformance table
             var foundIDs = db.UserSportPerformances
-                .Where(pf => pf.Sport.Equals(model.Sports.SportsName)
-                && pf.Level.Equals(model.Level.SportsLevel))
+                .Where(pf => pf.Sport.Equals(model.Sports.SportsName.ToString())
+                && pf.Level.Equals(model.Level.SportsLevel.ToString()))
                 ;
-
-            //foreach ()
-
-
-
-            if (!String.IsNullOrEmpty(model.Sports.SportsName.ToString()) && !String.IsNullOrEmpty(model.Level.SportsLevel.ToString()))
+            List<ApplicationUser> users = new List<ApplicationUser>();
+            foreach (var x in foundIDs)
             {
-
-                //            string searchSportString = model.Sports.SportsName.ToDisplayName().ToUpper();
-                //            string searchLevelString = model.Level.SportsLevel.ToDisplayName().ToUpper();
-
-                //            foundIDs = foundIDs.Where(s => s.Sport.ToUpper().Equals(searchSportString) && s.Level.ToUpper().Equals(searchLevelString));
-
-
-
-
-                //            //var result = from users in db.AspNetUsers
-                //            //           where users.userid in (
-                //            //                   from userperformance in db.UserSportPerformances
-                //            //                   where userperformance.level = model.level
-                //            //                   && userperformance.sport = model.sport
-                //            //                   select userperformance.UserID
-                //            //                   )
-                //            //            select user;
+                var user = await UserManager.FindByIdAsync(x.UserID);
+                if (user != null)
+                {
+                    users.Add(user);
+                }
+               
             }
-            else
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+            ViewBag.users = users;
+            return View("Step2");
 
-            //Prepare result
-            try
-            {
-                //            var preResult = new List<UserSportPerformance>();
-                //            if (foundIDs.Count() == 0)//No one match
-                //            {
-                //                //ViewBag.prompt = "No result found" + model.lng + "," + model.lat;
-                //                //return View("Index");
-                //            }
-                //            else //Find some ones's id matching
-                //            {
-                //                foreach (UserSportPerformance element in foundIDs)
-                //                {
-                //                    preResult.Add(element);
-                //                }
-                //            }
-                //            var jsonSerialiser = new JavaScriptSerializer();
-                //            var json = jsonSerialiser.Serialize(preResult);
-                //            ViewBag.result = json;
-                //            ViewBag.prompt = model.lng + "," + model.lat;
-                return View("Index");
-            }
-            catch (System.ComponentModel.Win32Exception e)
-            {
-                ViewBag.prompt = "System.ComponentModel.Win32Exception error";
-                return View("Index");
-            }
-            catch (System.Data.Entity.Core.EntityException e)
-            {
-                ViewBag.prompt = "System.Data.Entity.Core.EntityException error";
-                return View("Index");
-            }
+            
         }
 
 
